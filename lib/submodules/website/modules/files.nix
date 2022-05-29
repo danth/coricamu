3be015e2611433e9b1361508172bffe6ce620737
Daniel Thwaites <danthwaites30@btinternet.com>
2022-05-29 20:26:32 +0100

    Allow disabling minification :sparkles:

--- lib/submodules/website/modules/files.nix
+++ lib/submodules/website/modules/files.nix
@@ -5,19 +5,31 @@ with pkgsLib.types;
 with coricamuLib.types;
 
 {
-  options.files = mkOption {
-    description = "Attribute set containing files by path.";
-    type = attrsOf file;
-    default = {};
+  options = {
+    files = mkOption {
+      description = "Attribute set containing files by path.";
+      type = attrsOf file;
+      default = {};
+    };
+
+    minified = mkOption {
+      description = "Whether to minify files in the output.";
+      type = bool;
+      default = true;
+    };
   };
 
 
-  config.package = pkgs.runCommand "website-minified" {
-    source = pkgs.linkFarm "website"
-      (mapAttrsToList (name: path: { inherit name path; }) config.files);
-  } ''
-    cp --recursive --dereference --no-preserve=mode,ownership $source website
-    ${pkgs.minify}/bin/minify --all --sync --output minified --recursive website
-    mv minified/website $out
-  '';
+  config.package =
+    let
+      website = pkgs.linkFarm "website"
+        (mapAttrsToList (name: path: { inherit name path; }) config.files);
+
+      websiteMinified = pkgs.runCommand "website-minified" { } ''
+        cp --recursive --dereference --no-preserve=mode,ownership ${website} website
+        ${pkgs.minify}/bin/minify --all --sync --output minified --recursive website
+        mv minified/website $out
+      '';
+    in
+      if config.minified then websiteMinified else website;
 }
