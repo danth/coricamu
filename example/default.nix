danth/coricamu

commit 024b0b16208e184275fde8167f79c6b7b5f8b3fd

Process template tags within Nix :zap:
This removes the need to call out to an external Rust program
every time a template tag is encountered, therefore speeding
up evaluation. Furthermore, there is now no need to download a
Rust compiler and libraries the first time Coricamu is used on
a machine.

The code is currently quite disorganised. I originally intended
to match both the opening and closing tag as a single pattern, with
a capturing group for the contents between them. However, the type
of regular expressions which Nix supports doesn't include lazy
multipliers. This meant that if the same template was used multiple
times, we would match all of the stuff between the two template calls
as the contents of a single template, rather than correctly detecting
them as two separate ones.

Nix could do with switching to PCRE2 regular expressions to support
more exotic use cases such as this. I've saved the original code I
developed as a patch file, locally, so that it can be brought back if
this is ever done.

Anyway, there are still some bugs in this code:

- You can't nest a template as the contents of itself, because this
  case isn't currently handled.
- Invalid opening and closing of template tags also isn't handled.
  (For example, not closing a tag, or closing a tag when none was
  opened.) This should raise an error of some sort.

lib/fill-templates.nix
@@ -1,0 +1,110 @@
+{ coricamuLib, pkgsLib, pkgs, ... }:
+
+with pkgsLib;
+with coricamuLib;
+
+let
+  argumentPattern = ''([^[:space:]/>"'=]+)="(([^"\\]|\\.])*)"'';
+
+  matchArguments =
+    input:
+    let
+      matches = builtins.split argumentPattern input;
+      foldMatch =
+        accumulator: match:
+        if isString match
+        then accumulator
+        else accumulator // { "${elemAt match 0}" = elemAt match 1; };
+    in
+      foldl foldMatch {} matches;
+
+  templateStartPattern = name:
+    "<[[:space:]]*templates-${escapeRegex name}(([[:space:]]*${argumentPattern})*)[[:space:]]*>";
+
+  templateEndPattern = name:
+    "</[[:space:]]*templates-${escapeRegex name}[[:space:]]*>";
+
+  matchTemplate =
+    name: input:
+    let
+      splitted = builtins.split "(${templateStartPattern name}|${templateEndPattern name})" input;
+      isStartTag = match: isList match && elemAt match 2 != null;
+      isEndTag = match: isList match && elemAt match 2 == null;
+      collate =
+        collated: item:
+        if isStartTag item
+        then collated ++ [{
+          arguments = matchArguments (elemAt item 1);
+        }]
+        else
+          if isEndTag item
+          then collated
+          else
+            if (length collated == 0) || (last collated)?contents
+            then collated ++ [item]
+            else init collated ++ [(last collated // {
+              contents = item;
+            })];
+    in
+      foldl collate [] splitted;
+
+  mergeFillResults = left: right: {
+    body = left.body + right.body;
+    usedTemplates = left.usedTemplates ++ right.usedTemplates;
+  };
+
+  concatFillResults = foldl mergeFillResults {
+    body = "";
+    usedTemplates = [];
+  };
+
+  expandTemplate =
+    templates: template: arguments:
+    let
+      # We must repeat fillTemplates in case any templates were used
+      # within the output of the template.
+      filledTemplate = fillTemplates {
+        body = template.function arguments;
+        inherit templates;
+      };
+    in
+      {
+        body = filledTemplate.body;
+        usedTemplates = [ template ] ++ filledTemplate.usedTemplates;
+      };
+
+  fillTemplate =
+    templates: name: template: body:
+    concatFillResults (
+      map
+      (item:
+        if isString item
+        then {
+          body = item;
+          usedTemplates = [];
+        }
+        else
+          expandTemplate templates template
+          (item.arguments // (optionalAttrs (item.contents != "") { inherit (item) contents; }))
+      )
+      (matchTemplate name body)
+    );
+
+  fillTemplates =
+    { body, templates }:
+    let
+      fillers = mapAttrsToList (
+        name: template: result:
+        let newResult = fillTemplate templates name template result.body;
+        in {
+          body = newResult.body;
+          usedTemplates = result.usedTemplates ++ newResult.usedTemplates;
+        }
+      ) templates;
+    in
+      pipe {
+        inherit body;
+        usedTemplates = [];
+      } fillers;
+
+in { inherit fillTemplates; }

lib/default.nix
@@ -1,5 +1,5 @@
 args:
-(import ./fill-templates/lib.nix args)
+(import ./fill-templates.nix args)
 // (import ./markdown.nix args)
 // (import ./submodules/page/lib.nix args)
 // (import ./submodules/website/lib.nix args)

lib/submodules/page/modules/default.nix
@@ -8,7 +8,6 @@ with coricamuLib.types;
 let
   filledTemplates = fillTemplates {
     inherit (websiteConfig) templates;
-    name = config.path;
     body = ''
       ${
         optionalString

flake.nix
@@ -33,10 +33,7 @@
       # libOutputs contains the value of `«Coricamu's flake».lib`,
       # which is used to build the example website.
 
-      libOutputs = mergeOutputs (map callOutputs [
-        ./lib/flake-tools.nix
-        ./lib/fill-templates/outputs.nix
-      ]);
+      libOutputs = callOutputs ./lib/flake-tools.nix;
 
       exampleOutputs = libOutputs.lib.generateFlakeOutputs {
         outputName = "example";

lib/fill-templates/Cargo.toml
@@ -1,10 +1,0 @@
-[package]
-name = "fill-templates"
-description = "Convert HTML into Nix code which calls Coricamu templates"
-license = "MIT"
-publish = false
-version = "1.0.1"
-edition = "2021"
-
-[dependencies]
-kuchiki = "0.8"

lib/fill-templates/lib.nix
@@ -1,47 +1,0 @@
-{ coricamuLib, pkgsLib, pkgs, ... }:
-
-with pkgsLib;
-with coricamuLib;
-
-{
-  fillTemplates =
-    { name, body, templates, phase ? 1, usedTemplates ? [] }:
-    let
-      inherit (splitFilename name) baseName;
-      nixName = "${baseName}-templates-phase${toString phase}.nix";
-
-      nixFile = pkgs.runCommand nixName {
-        inherit body;
-        passAsFile = [ "body" ];
-
-        # This is import-from-derivation, and is needed every time a user wants
-        # to preview the site, so must be built quickly.
-        preferLocalBuild = true;
-        allowSubstitutes = false;
-      } ''
-        ${pkgs.coricamu.fill-templates}/bin/fill-templates <$bodyPath >$out
-      '';
-
-      wrappedTemplates = mapAttrs' (templateName: template: {
-        # HTML tags are case-insensitive, so we convert the name to lowercase
-        name = toLower templateName;
-        value = template;
-      }) templates;
-
-      # If this string isn't present, template tags are definitely not used,
-      # so the import-from-derivation can be skipped.
-      mayContainTemplateTag = hasInfix "<templates-" body;
-
-    in if mayContainTemplateTag
-       then let
-         output = (import nixFile) wrappedTemplates;
-       in fillTemplates {
-         inherit name templates;
-         inherit (output) body;
-         phase = phase + 1;
-         usedTemplates = usedTemplates ++ output.usedTemplates;
-       }
-       else {
-         inherit usedTemplates body;
-       };
-}

lib/fill-templates/outputs.nix
@@ -1,25 +1,0 @@
-{ utils, crane, ... }:
-
-utils.lib.eachDefaultSystem (
-  system:
-
-  with crane.lib.${system};
-
-  let
-    src = ./.;
-
-    cargoArtifacts = buildDepsOnly {
-      inherit src;
-    };
-
-  in {
-    packages.fill-templates = buildPackage {
-      inherit src cargoArtifacts;
-    };
-
-    checks.fill-templates-clippy = cargoClippy {
-      inherit src cargoArtifacts;
-      cargoClippyExtraArgs = "-- --deny warnings";
-    };
-  }
-)

lib/fill-templates/src/main.rs
@@ -1,126 +1,0 @@
-extern crate kuchiki;
-
-use kuchiki::{parse_html, NodeRef};
-use kuchiki::traits::*;
-use std::collections::HashSet;
-use std::io;
-
-fn escape_nix_string(content: &str) -> String {
-    // A multiline string is used as it only has two things to escape.
-    content.replace("''", "'''").replace("${", "''${")
-}
-
-fn call_template(
-    template_calls: &mut Vec<String>,
-    template_name: &str,
-    template_arguments: &str
-) -> String {
-    let template_number = template_calls.len();
-    let template_call = format!(
-        "templates.{}.function {}",
-        template_name, template_arguments
-    );
-    template_calls.push(template_call);
-    format!("${{template{}}}", template_number)
-}
-
-fn process_node(
-    used_templates: &mut HashSet<String>,
-    template_calls: &mut Vec<String>,
-    node: &NodeRef
-) {
-    if let Some(element) = node.as_element() {
-        let name = element.name.local.to_string();
-        if let Some(template_name) = name.strip_prefix("templates-") {
-            used_templates.insert(template_name.to_string());
-
-            let mut template_arguments = String::from("{\n");
-
-            // Add content to the template arguments
-            let mut content_string = "".to_string();
-
-            for child in node.children() {
-                content_string.push_str(&child.to_string());
-            }
-            if !content_string.trim().is_empty() {
-                let content_string = escape_nix_string(&content_string);
-                let content_argument = format!("contents = ''{}'';\n", content_string);
-                template_arguments.push_str(&content_argument);
-            }
-
-            // Add attributes to the template arguments
-            for (attribute_name, attribute)
-            in &element.attributes.borrow().map {
-                let attribute_name = attribute_name.local.to_string();
-                let attribute_value = escape_nix_string(&attribute.value);
-                let argument = format!("{} = ''{}'';\n", attribute_name, attribute_value);
-                template_arguments.push_str(&argument);
-            }
-
-            template_arguments.push('}');
-
-            let splice = call_template(
-                template_calls, template_name, &template_arguments
-            );
-            let splice = NodeRef::new_text(splice);
-            node.insert_after(splice);
-            node.detach();
-            return;
-        }
-
-        for attribute in element.attributes.borrow_mut().map.values_mut() {
-            let escaped_value = escape_nix_string(&attribute.value);
-            attribute.value.clear();
-            attribute.value.push_str(&escaped_value);
-        }
-    }
-
-    if let Some(text) = node.as_text() {
-        let escaped_text = escape_nix_string(&text.take());
-        let escaped_node = NodeRef::new_text(escaped_text);
-        node.insert_after(escaped_node);
-        node.detach();
-        return;
-    }
-
-    for child in node.children() {
-        process_node(used_templates, template_calls, &child);
-    }
-}
-
-fn main() {
-    // Read and parse HTML from stdin
-    let document = parse_html()
-        .from_utf8()
-        .read_from(&mut io::stdin().lock())
-        .expect("Parsing input")
-        // The input is automatically wrapped in <html>, which we don't want
-        .children().next()
-        .expect("Selecting document body")
-        .children().nth(1)
-        .expect("Selecting document body");
-
-    // Fill templates
-    let mut used_templates = HashSet::new();
-    let mut template_calls = Vec::new();
-    process_node(&mut used_templates, &mut template_calls, &document);
-
-    // Write Nix code to stdout
-    println!("templates:");
-    if !template_calls.is_empty() {
-        println!("let");
-        for (template_number, template_call) in template_calls.iter().enumerate() {
-            println!("template{} = {};", &template_number, &template_call);
-        }
-        println!("in {{");
-    } else {
-        println!("{{");
-    }
-    print!("usedTemplates = [");
-    for template_name in &used_templates {
-        print!("templates.{} ", &template_name);
-    }
-    println!("];");
-    println!("body = ''{}'';", &document.to_string());
-    println!("}}");
-}

commit 181866d12168041336aeaf753f7a3dafbf2130e4

Support nested template calls :bug:
This was a known regression introduced by
024b0b16208e184275fde8167f79c6b7b5f8b3fd.

example/default.nix
@@ -12,6 +12,9 @@
       <templates-font-awesome icon="book" style="solid"></templates-font-awesome>
       This is an example website for
       <a href="https://github.com/danth/coricamu">Coricamu</a>.
+      <templates-note title="Nesting">
+        You can nest templates!
+      </templates-note>
     </templates-note>
   '';
 

lib/fill-templates.nix
@@ -41,19 +41,65 @@ let
       isStartTag = match: isList match && elemAt match 2 != null;
       isEndTag = match: isList match && elemAt match 2 == null;
       collate = collated: item:
-        if isStartTag item
-        then collated ++ [{
-          arguments = matchArguments (elemAt item 1);
-        }]
-        else
-          if isEndTag item
-          then collated
+        let
+          # Operations on the output value.
+          # We can either add a new match object, or update the latest one.
+          append = value: collated ++ [value];
+          updateLast = attrs: init collated ++ [(last collated // attrs)];
+
+          # If true, we have not reached a closing tag for the current call yet.
+          lastIsOpen =
+            (length collated > 0) &&
+            isAttrs (last collated) &&
+            (last collated).open > 0;
+        in
+          if isStartTag item
+          then
+            if lastIsOpen
+            # There is already a template open, so we convert another opening
+            # tag back to a string. This string will be passed to the already
+            # open template as its contents, and possibly returned to us later,
+            # when it will be parsed as a template again. This process allows
+            # template calls to be nested.
+            then updateLast {
+              contents = (last collated).contents + (elemAt item 0);
+              # We must count how many times we have seen a nested opening tag
+              # so that the corresponding number of closing tags can be processed.
+              open = (last collated).open + 1;
+            }
+            # There is no template open, so we open a new one.
+            else append {
+              open = 1;
+              arguments = matchArguments (elemAt item 1);
+              contents = "";
+            }
           else
-            if (length collated == 0) || (last collated)?contents
-            then collated ++ [item]
-            else init collated ++ [(last collated // {
-              contents = item;
-            })];
+            if isEndTag item
+            then
+              if (last collated).open > 1
+              # This closing tag corresponds to an opening tag which was nested,
+              # therefore is is converted to a string as per the comment above.
+              then updateLast {
+                contents = (last collated).contents + (elemAt item 0);
+                # Count how many times we have seen a nested closing tag so that
+                # we know when the template should be finished.
+                open = (last collated).open - 1;
+              }
+              # This is a normal closing tag.
+              else updateLast {
+                # We know that open <= 1, so we can skip decrementing and simply
+                # set it to zero.
+                open = 0;
+              }
+            else
+              # This is a string of content, not a relevant template tag.
+              if !lastIsOpen
+              # Between calls, we just insert content to the output list directly.
+              then append item
+              # Within a template call, we must add to the content of the template.
+              else updateLast {
+                contents = (last collated).contents + item;
+              };
     in
       foldl collate [] splitted;
 