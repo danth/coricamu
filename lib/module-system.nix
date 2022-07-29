304562b70c4c51c0ba2bd2106703d5423eaee139
Daniel Thwaites <danthwaites30@btinternet.com>
2022-07-29 13:54:36 +0100

    Format type and default as a table :recycle:

--- lib/module-system.nix
+++ lib/module-system.nix
@@ -4,17 +4,10 @@ with coricamuLib;
 with pkgsLib;
 
 {
-  makeOptionDocBook = option: ''
-    <section>
-      <title>${escapeXML option.name}</title>
-
-      ${optionalString (option.description != null) ''
-        <para>${option.description}</para>
-      ''}
-
-      <para>Type: ${option.type}</para>
-
-      <para>Default: ${
+  makeOptionDocBook =
+    option:
+    let
+      default =
         if option?defaultText
         then
           if isString option.defaultText
@@ -25,14 +18,33 @@ with pkgsLib;
             else
               if option.defaultText._type == "literalExpression"
               then "<literal>${escapeXML (option.defaultText.text)}</literal>"
-              else option.defaultText
+              else throw "Text type ${option.defaultText._type} is not implemented"
         else
           if option?default
           then "<literal>${escapeXML (showVal option.default)}</literal>"
-          else "undefined"
-      }</para>
-    </section>
-  '';
+          else null;
+    in ''
+      <section>
+        <title>${escapeXML option.name}</title>
+
+        <table><tgroup><tbody>
+          <row>
+            <entry>Type:</entry>
+            <entry>${option.type}</entry>
+          </row>
+          ${optionalString (default != null) ''
+            <row>
+              <entry>Default:</entry>
+              <entry>${default}</entry>
+            </row>
+          ''}
+        </tbody></tgroup></table>
+
+        ${optionalString (option.description != null) ''
+          <para>${option.description}</para>
+        ''}
+      </section>
+    '';
 
   makeOptionsDocBook = {
     options,
