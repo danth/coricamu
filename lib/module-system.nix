ce7b4aa39f74d7789d53c4118a5fbde5dec1e5e1
Daniel Thwaites <danthwaites30@btinternet.com>
2022-07-31 13:34:19 +0100

    Decrease heading level of nested options :sparkles:

--- lib/module-system.nix
+++ lib/module-system.nix
@@ -3,86 +3,101 @@
 with coricamuLib;
 with pkgsLib;
 
-{
-  makeOptionDocBook =
-    option:
-    let
-      formatVerbatim = text:
-        if hasInfix "\n" text
-        then "<programlisting>${escapeXML text}</programlisting>"
-        else "<literal>${escapeXML text}</literal>";
+let
+  formatVerbatim = text:
+    if hasInfix "\n" text
+    then "<programlisting>${escapeXML text}</programlisting>"
+    else "<literal>${escapeXML text}</literal>";
 
-      formatValue = value: formatVerbatim (showVal value);
+  formatValue = value: formatVerbatim (showVal value);
 
-      formatParagraph = text:
-        if builtins.match ".*</[a-z]+>.*" text != null
-        then "<para>${text}</para>"
+  formatParagraph = text:
+    if builtins.match ".*</[a-z]+>.*" text != null
+    then "<para>${text}</para>"
+    else
+      let splitText = replaceStrings [ "\n\n" ] [ "</para><para>" ] text;
+      in "<para>${splitText}</para>";
+
+  formatAnything = fallback: value:
+    if value?_type
+    then
+      if value._type == "literalDocBook"
+      then formatParagraph value.text
+      else
+        if value._type == "literalExpression"
+        then formatVerbatim value.text
         else
-          let splitText = replaceStrings [ "\n\n" ] [ "</para><para>" ] text;
-          in "<para>${splitText}</para>";
+          warn "Text type `${value._type}` is not implemented"
+          (formatVerbatim value.text)
+    else fallback value;
 
-      formatAnything = fallback: value:
-        if value?_type
-        then
-          if value._type == "literalDocBook"
-          then formatParagraph value.text
-          else
-            if value._type == "literalExpression"
-            then formatVerbatim value.text
-            else
-              warn "Text type `${value._type}` is not implemented"
-              (formatVerbatim value.text)
-        else fallback value;
+  formatValue' = formatAnything formatValue;
+  formatParagraph' = formatAnything formatParagraph;
 
-        formatValue' = formatAnything formatValue;
-        formatParagraph' = formatAnything formatParagraph;
-
-    in ''
-      <section>
-        <title>${escapeXML option.name}</title>
-
-        <table><tgroup><tbody>
-          <row>
-            <entry>Type:</entry>
-            <entry>${option.type}</entry>
-          </row>
-          ${optionalString (option?default) ''
-            <row>
-              <entry>Default:</entry>
-              <entry>${formatValue' option.default}</entry>
-            </row>
-          ''}
-          ${optionalString (option?example) ''
-            <row>
-              <entry>Example:</entry>
-              <entry>${formatValue' option.example}</entry>
-            </row>
-          ''}
-        </tbody></tgroup></table>
-
-        ${optionalString (option.description != null) ''
-          ${formatParagraph' option.description}
-        ''}
-      </section>
-    '';
-
+in {
   makeOptionsDocBook = {
     options,
     showInvisible ? false,
     showInternal ? false,
     customFilter ? (option: true)
-  }: ''
-    <section xmlns:xlink="http://www.w3.org/1999/xlink">
-      ${pipe options [
-        optionAttrSetToDocList
-        (filter (option:
-          (customFilter option) &&
-          (option.visible || showInvisible) &&
-          (!option.internal || showInternal)
-        ))
-        (map makeOptionDocBook)
-        (concatStringsSep "\n")
-      ]}
+  }:
+  let
+    makeOptionDocBook =
+      option:
+      let
+        subOptions = option.type.getSubOptions option.loc;
+      in ''
+        <section xmlns:xlink="http://www.w3.org/1999/xlink">
+          <title>${escapeXML (showOption option.loc)}</title>
+
+          <table><tgroup><tbody>
+            <row>
+              <entry>Type:</entry>
+              <entry>${option.type.description or "unspecified"}</entry>
+            </row>
+            ${optionalString (option?defaultText || option?default) ''
+              <row>
+                <entry>Default:</entry>
+                <entry>${formatValue' (option.defaultText or option.default)}</entry>
+              </row>
+            ''}
+            ${optionalString (option?example) ''
+              <row>
+                <entry>Example:</entry>
+                <entry>${formatValue' option.example}</entry>
+              </row>
+            ''}
+          </tbody></tgroup></table>
+
+          ${optionalString (option.description != null) ''
+            ${formatParagraph' option.description}
+          ''}
+
+          ${
+            optionalString (subOptions != {})
+            (concatStringsSep "\n" (makeOptionsDocBooks subOptions))
+          }
+        </section>
+      '';
+
+    makeOptionsDocBooks = options: pipe options [
+      attrValues
+      (map (option: 
+        if isOption option
+        then
+          if (customFilter option) &&
+            ((option.visible or true) || showInvisible) &&
+            (!(option.internal or false) || showInternal)
+          then [ (makeOptionDocBook option) ]
+          else []
+        else makeOptionsDocBooks option
+      ))
+      concatLists
+    ];
+
+  in ''
+    <section>
+      ${concatStringsSep "\n" (makeOptionsDocBooks options)}
     </section>
   '';
 }
