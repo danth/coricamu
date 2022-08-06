9c3461d99d19c30cdde51560acdd2b022e677dbb
Daniel Thwaites <danthwaites30@btinternet.com>
2022-08-07 00:57:08 +0100

    Defer loading of Mermaid script :zap:

--- lib/submodules/website/modules/mermaid.nix
+++ lib/submodules/website/modules/mermaid.nix
@@ -6,20 +6,13 @@
       { contents }:
       "<pre class=\"mermaid\">${contents}</pre>";
 
-    scripts = [
-      {
-        path = "mermaid/mermaid@8.14.0.js";
-        javascriptFile = pkgs.fetchurl {
-          url = "https://cdn.jsdelivr.net/npm/mermaid@8.14.0/dist/mermaid.js";
-          sha256 = "HgkOi3/ypnDigo/pp/J1Bxvf5mY4hqvDW+YrxdqfMIM=";
-        };
-      }
-      {
-        path = "mermaid/initialise.js";
-        javascript = ''
-          mermaid.initialize({ startOnLoad: true });
-        '';
-      }
-    ];
+    scripts = [{
+      path = "mermaid@8.14.0.js";
+      javascriptFile = pkgs.fetchurl {
+        url = "https://cdn.jsdelivr.net/npm/mermaid@8.14.0/dist/mermaid.js";
+        sha256 = "HgkOi3/ypnDigo/pp/J1Bxvf5mY4hqvDW+YrxdqfMIM=";
+      };
+      defer = true;
+    }];
   };
 }
