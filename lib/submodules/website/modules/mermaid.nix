42481f63d00fb7c0c107e9de23cde66d0bb1d4d5
Daniel Thwaites <danthwaites30@btinternet.com>
2022-10-10 09:15:24 +0100

    Update Mermaid to v9.1.7 :arrow_up:

--- lib/submodules/website/modules/mermaid.nix
+++ lib/submodules/website/modules/mermaid.nix
@@ -7,10 +7,10 @@
       "<pre class=\"mermaid\">${contents}</pre>";
 
     scripts = [{
-      path = "mermaid@9.1.4.js";
+      path = "mermaid@9.1.7.js";
       javascriptFile = pkgs.fetchurl {
-        url = "https://cdn.jsdelivr.net/npm/mermaid@9.1.4/dist/mermaid.js";
-        sha256 = "bq7V70Dsp2rvYbu/KJA//TPzdrtRTdwl06OWqJiOglg=";
+        url = "https://cdn.jsdelivr.net/npm/mermaid@9.1.7/dist/mermaid.js";
+        sha256 = "5ZEQFGiF7DmrBf8P7c5EcsOUldgHNKU0hTzGiEhjGc4=";
       };
       defer = true;
     }];
